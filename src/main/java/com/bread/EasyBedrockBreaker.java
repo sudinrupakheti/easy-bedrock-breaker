1cfac598d18827990779558238fb697230b41c0e
Sudin Rupakheti <sudinrupakheti977@gmail.com>
2025-12-21 14:55:23 +0545

    Replace deprecated HudElementRegistry with HudRenderCallback

--- src/main/java/com/bread/EasyBedrockBreaker.java
+++ src/main/java/com/bread/EasyBedrockBreaker.java
@@ -3,14 +3,13 @@ package com.bread;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.*;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,31 +34,27 @@ public class EasyBedrockBreaker implements ClientModInitializer {
 			"key.bread.delayBlockPackets", 
 			InputUtil.Type.KEYSYM, 
 			InputUtil.UNKNOWN_KEY.getCode(), 
-			"category.bread.breadclient"
+			KeyBinding.MISC_CATEGORY
 		));
 		
 		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			if (!activateKey.isPressed()) releasePackets();
 		});
 		
-		// Using HudElementRegistry instead of deprecated HudRenderCallback
-		HudElementRegistry.attachElementAfter(
-			null,
-			Identifier.of("easy-bedrock-breaker", "packet_delay_indicator"),
-			(drawContext, tickCounter) -> {
-				if (EasyBedrockBreaker.isDelayingPackets()) {
-					MinecraftClient client = MinecraftClient.getInstance();
-					drawContext.drawText(
-						client.textRenderer,
-						"delaying packets",
-						4,
-						drawContext.getScaledWindowHeight() - 4 - client.textRenderer.fontHeight,
-						0xFFFFFFFF,
-						true
-					);
-				}
+		// Using HudRenderCallback (still works in 1.21.11)
+		HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
+			if (EasyBedrockBreaker.isDelayingPackets()) {
+				MinecraftClient client = MinecraftClient.getInstance();
+				drawContext.drawText(
+					client.textRenderer,
+					"delaying packets",
+					4,
+					drawContext.getScaledWindowHeight() - 4 - client.textRenderer.fontHeight,
+					0xFFFFFFFF,
+					true
+				);
 			}
-		);
+		});
 		
 		LOGGER.info("easy bedrock breaker initialized");
 	}
